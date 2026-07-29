acd469ff8f3609be5bed25ae74d8de407e053c5f
Paulo Lara <lar.paulo.an@gmail.com>
2026-07-28 21:32:37 -0600

    Deep.py is done, this one took a while

---
+++ Week-1/deep.py
@@ -0,0 +1,16 @@
+
+def stringorint():
+    Question = input("What is the answer to the great question of life, the universe, and everything? ")
+    try:
+        return int(Question)
+    except ValueError:
+        return Question
+
+def main():
+    respuesta = stringorint()
+    match respuesta:
+        case 42 | "forty-two" | "forty two":
+            print("Yes")
+        case _:
+            print("No")
+main()
