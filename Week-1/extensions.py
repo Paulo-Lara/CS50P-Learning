966a167ac9fa440e9d8661626c5e703b30c4da82
Paulo Lara <lar.paulo.an@gmail.com>
2026-07-30 17:57:43 -0600

    After a lot of hours of working, extensions is done (CHECK COMMENTS)

--- Week-1/extensions.py
+++ Week-1/extensions.py
@@ -1,6 +1,39 @@
-#Poner en el input ("File name = input....")
-#Busca en el internet como es el formato de cada cosa en la pagina web, por ejemplo:
-#documento.pdf = application/pdf
 
-#Haz que el programa lea después de lo que diga el punto (.), y así, determinar que
-#poner en el output
+#I had to learn for i in range from 20 different tutorials just because I know I will need this in the future
+#It did end up working out but the logic took ages to complete
+#I know a little bit of C language so thats why it gave me an advantage to use the range function
+archivo = input("File name: ")
+
+punto = 0
+
+for i in range(len(archivo)):
+    if archivo[i] == ".":
+        punto = i
+
+final = archivo[punto :].lower().strip()
+
+match final:
+    case ".jpeg" | ".jpg":
+        print("image/jpeg")
+
+    case ".gif":
+        print("image/gif")
+
+    case ".png":
+        print("image/png")
+
+    case ".pdf":
+        print("application/pdf")
+
+    case ".txt":
+        print("text/plain")
+
+    case ".zip":
+        print("application/zip")
+
+    case ".bin":
+        print("application/octet-stream")
+
+    case _:
+        print("application/octet-stream")
+
