321f5b747934bccffc39fa00250fe9fc7da37914
Paulo Lara <lar.paulo.an@gmail.com>
2026-07-29 21:17:44 -0600

    Thoughs on the logic but in spanish

---
+++ Week-1/extensions.py
@@ -0,0 +1,6 @@
+#Poner en el input ("File name = input....")
+#Busca en el internet como es el formato de cada cosa en la pagina web, por ejemplo:
+#documento.pdf = application/pdf
+
+#Haz que el programa lea después de lo que diga el punto (.), y así, determinar que
+#poner en el output
