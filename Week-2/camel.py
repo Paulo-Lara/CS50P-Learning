8dbf4bea8c54a918c95574407c7b74ca993019dd
Paulo Lara <lar.paulo.an@gmail.com>
2026-08-07 17:58:58 -0600

    Here is camel.py! Some of the logic used in extensions.py from week-1 was really useful

--- Week-2/camel.py
+++ Week-2/camel.py
@@ -1,1 +1,11 @@
 #Camel case = When there is a capitalized letter, an "_" must be on snake_case before that capitalized letter
+#This is why in extensions.py i used for loop, I knew I was gonna use it
+camel_case = input("camelCase: ")
+
+mayus = 0
+for letra in camel_case:
+    if letra.isupper() == True:
+        mayus = letra.lower()
+        print("_" + mayus, end="")
+    else:
+        print(letra, end="")
